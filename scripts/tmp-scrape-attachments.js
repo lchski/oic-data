87e82c38002186f5260ae0c1daa03f7e7835c706
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-07-06 16:59:45 -0400

    pull attachment URLs into array

---
+++ scripts/tmp-scrape-attachments.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const savedOrderTablesPath = 'order-tables/';
+const savedOrderTables = fs.readdirSync(savedOrderTablesPath).filter(filename => filename !== ".gitkeep").filter(filename => filename.endsWith("json"));
+
+
+
+function listAttachmentUrls() {
+	let attachmentUrls = [];
+
+	for (let orderTableFilename of savedOrderTables) {
+		let orderTablePath = savedOrderTablesPath + orderTableFilename;
+	
+		let orderTable = JSON.parse(fs.readFileSync(orderTablePath));
+	
+		attachmentUrls = [...new Set([...orderTable.attachments, ...attachmentUrls])].sort();
+	
+		if (orderTable.attachments.length > 0) {
+			fs.writeFileSync('attachment-urls.json', JSON.stringify(attachmentUrls, null, 2));
+		}
+		console.log(`${orderTablePath} - ${orderTable.attachments.length} attachments`);
+	}
+
+	fs.writeFileSync('attachment-urls.json', JSON.stringify(attachmentUrls, null, 2));
+}
+listAttachmentUrls();
+
