ecbd5b9867b03a39c44bc2ee98a703ed9f289742
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-07-06 14:16:46 -0400

    smarter attachment script (allow for stop / restart)

--- scripts/tmp-extract-attachment-urls.js
+++ scripts/tmp-extract-attachment-urls.js
@@ -14,8 +14,16 @@ function extractAttachmentUrls(orderTable) {
 		.filter(linkHref => linkHref.includes('attachment.php'));
 }
 
-for (let orderTablePath in savedOrderTables) {
+for (let orderTableFilename of savedOrderTables) {
+	let orderTablePath = savedOrderTablesPath + orderTableFilename;
+
 	let orderTable = JSON.parse(fs.readFileSync(orderTablePath));
+
+	// Bail if this file already has an `attachments` property
+	if (orderTable.hasOwnProperty('attachments')){
+		console.log(`skipping ${orderTablePath}`)
+		continue;
+	}
 	
 	fs.writeFileSync(
 		orderTablePath,
