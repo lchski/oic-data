4957b4ce856b262eaa370ea18a59b9795a3373c9
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-12-12 16:19:40 -0500

    collect extra info when scraping tables
    (push this up to data collection, vs analysis repo; need to do existing
    tables, somehow, a one-time run)

--- scripts/lib/scraping.js
+++ scripts/lib/scraping.js
@@ -8,10 +8,53 @@ const attachmentIdsPath = 'attachment-ids.json';
 // NB: run within the puppeteer Chrome instance—doesn't have access to script scope
 async function extractOrderTables() {
 	orderTables = Array.from(document.querySelectorAll('main > form > table'));
-	
+
+	function textFromCellReference(node, tr_num, td_num) {
+		return node.querySelector('tr:nth-of-type(' + tr_num + ') > td:nth-of-type(' + td_num + ')').innerText;
+	}
+
+	function textFromXpath(node, xpath) { // xpath must start with './/' to be inside `node` context
+		return document.evaluate(xpath, node, null, XPathResult.STRING_TYPE).stringValue;
+	}
+
+	function registrationFromNode(node) {
+		return textFromXpath(node, './/td[text()="Registration"]/following-sibling::td[@colspan=5 or @id="registration"]')
+	}
+
+	function parseRegistration(registrationText) {
+		let registrationRegex = "Registration: ([A-Z]*) ?\/(?: ([0-9]{4}-[0-9]{4})  Publication Date: ([0-9]{4}-[0-9]{2}-[0-9]{2}))?";
+
+		let parsedRegistration = registrationText.match(registrationRegex);
+
+		if (null == parsedRegistration) {
+			return {
+				type: '',
+				id: '',
+				publication_date: '',
+			}
+		}
+
+		return {
+			type: parsedRegistration[1],
+			id: null == parsedRegistration[2] ? '' : parsedRegistration[2],
+			publication_date: null == parsedRegistration[3] ? '' : parsedRegistration[3],
+		}
+	}
+
 	return orderTables.map((tableNode) => ({
 		html: tableNode.outerHTML,
-		pcNumber: tableNode.querySelector('tr:nth-of-type(2) > td:nth-of-type(2)').innerText,
+		pcNumber: textFromCellReference(tableNode, 2, 2),
+		date: textFromCellReference(tableNode, 2, 3),
+		chapter: textFromCellReference(tableNode, 2, 4),
+		bill: textFromCellReference(tableNode, 2, 5),
+		department: textFromCellReference(tableNode, 2, 6),
+		act: textFromXpath(tableNode, './/td[text()="Act"]/following-sibling::td[@colspan=5]'),
+		subject: textFromXpath(tableNode, './/td[text()="Subject"]/following-sibling::td[@colspan=5]'),
+		precis: textFromXpath(tableNode, './/td[text()="Precis"]/following-sibling::td[@colspan=5 or @id="precis"]'),
+		registration: registrationFromNode(tableNode),
+		registration_type: parseRegistration(registrationFromNode(tableNode))['type'],
+		registration_id: parseRegistration(registrationFromNode(tableNode))['id'],
+		registration_publication_date: parseRegistration(registrationFromNode(tableNode))['publication_date'],
 		attachments: Array.from(
 			tableNode.querySelectorAll('a'))
 				.map(linkNode => linkNode.href.replace(linkNode.origin, "").replace("/", "")) // get just "attachment.php" and on
